f616296f64f926a6adb2a8fc1077c86f6f83c53a
Fadhil Muhammad <fadhilmuhammad.fd@gmail.com>
2023-10-27 10:04:52 +0700

    review buku, sedikit ajh

--- landing_page/urls.py
+++ landing_page/urls.py
@@ -6,5 +6,7 @@ app_name = "landing_page"
 urlpatterns = [
     path("", get_books, name="get_books"),
     path("page/", show_landing_page, name="show_landing_page"),
-    path("get_data_json/", get_data_json, name="get_data_json")
+    path("get_data_json/", get_data_json, name="get_data_json"),
+    path("book/review/data/<int:book_id>/", get_review_data, name="get_review_data"),
+    path("book/review/<int:book_id>/", show_review, name="show_review"),
 ]
