80822ab8d7d6d3203a48cb0379974b6da9eba940
nevadenstia <neva.denstia@ui.ac.id>
2023-10-29 11:33:25 +0700

    Finished Comments section and Top Voted Books

--- readers_favorite/urls.py
+++ readers_favorite/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from landing_page.views import get_data_json
 
-from readers_favorite.views import add_comment, add_upvote_ajax, get_user_info, show_readers_favorite
+from readers_favorite.views import add_comment, add_upvote_ajax, get_all_comments, get_commenters, show_readers_favorite
 
 app_name = 'readers_favorite'
 
@@ -9,8 +9,7 @@ urlpatterns = [
     path('', show_readers_favorite, name='show_readers_favorite'),
     path("get_data_json/", get_data_json, name="get_data_json"),
     path('add_upvote_ajax/<int:item_id>/', add_upvote_ajax, name='add_upvote_ajax'),
-    path('get_user_info/', get_user_info, name='get_user_info'),
+    path('get_all_comments/', get_all_comments, name='get_all_comments'),
+    path('get_commenters/', get_commenters, name='get_commenters'),
     path('add_comment/', add_comment, name='add_comment'),
-
-
 ]
--- readers_favorite/views.py
+++ readers_favorite/views.py
@@ -1,20 +1,28 @@
 import json
-from django.http import HttpResponse, HttpResponseNotFound
+from django.db.models import F
+from django.http import HttpResponse, HttpResponseNotFound, JsonResponse
 from django.shortcuts import get_object_or_404, render
 from landing_page.models import Books
-from readers_favorite.models import ReadersFavorite
+from readers_favorite.models import ReadersFavorite, Comments
 from django.core import serializers
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
+from django.db.models import Sum
 
 def show_readers_favorite(request):
     books = Books.objects.all()
-    user_info = ReadersFavorite.objects.all()
 
     if request.user.is_authenticated:
+        user = request.user  # Get the current user
+        all_comments = Comments.objects.all()
+        user_contribution = Comments.objects.filter(user=user).count()
+
+        # Try to get the user's existing ReadersFavorite or create a new one
+        user_selected, created = ReadersFavorite.objects.get_or_create(user=user, user_contribution = user_contribution)
+
         response = {
             'books': books,
-            'user_info':user_info,
+            'all_comments': all_comments,
         }
         return render(request, "readers_favorite.html", response)
     else:
@@ -24,7 +32,11 @@ def show_readers_favorite(request):
         return render(request, "readers_favorite_base.html", response)
 
 
-def get_user_info(request):
+def get_all_comments(request):
+    data = Comments.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def get_commenters(request):
     data = ReadersFavorite.objects.all()
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
@@ -45,9 +57,11 @@ def add_comment(request):
     if request.method == 'POST':
         user_comment = request.POST.get("user_comment")
         user = request.user
-        new_comment = ReadersFavorite(user_comment=user_comment, user=user)
-        new_comment.user_contribution += 1
+        new_comment = Comments(user=user, user_comment=user_comment)
         new_comment.save()
+
+        # Increment user_contribution by 1 for the current user
+        ReadersFavorite.objects.filter(user=user).update(user_contribution=F('user_contribution') + 1)
 
         return HttpResponse(b"CREATED", status=201)
     
