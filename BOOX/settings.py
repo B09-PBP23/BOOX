b07e71379712dd3b290d340a910d92098fb069e5
Theodore Kasyfillah <124960077+tedskiii@users.noreply.github.com>
2023-12-06 14:07:42 +0700

    Update settings.py

--- BOOX/settings.py
+++ BOOX/settings.py
@@ -30,7 +30,7 @@ PRODUCTION = env.bool('PRODUCTION', False)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
