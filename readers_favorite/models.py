6060787e1cdfc683783fded41884b23221d7b8a0
Theodore Kasyfillah <124960077+tedskiii@users.noreply.github.com>
2023-12-16 18:14:33 +0700

    Update models.py

--- readers_favorite/models.py
+++ readers_favorite/models.py
@@ -1,15 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
-from landing_page.models import Books 
 
 class ReadersFavorite(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     user_contribution = models.IntegerField(default=0)
-
-class Upvote(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    book = models.ForeignKey(Books, on_delete=models.CASCADE)
 
 class Comments(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
